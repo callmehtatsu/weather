62646e2caae6069fa164921afc5cf6104c2fe56c
callmehtatsu <24110359@vnu.edu.vn>
2025-12-14 06:44:10 +0700

    feat: them constants

---
+++ src/constants/styles.js
@@ -0,0 +1,7 @@
+﻿
+export const METRICS_COLORED_SHADOW = '0px 8px 20px rgba(25, 118, 210, 0.3), 0px 3px 10px rgba(25, 118, 210, 0.25)';
+export const ACTIVE_HOURLY_COLORED_SHADOW = '0px 8px 20px rgba(25, 118, 210, 0.4), 0px 3px 10px rgba(25, 118, 210, 0.3)';
+
+
+export const GLASS_BACKGROUND = 'rgba(255, 255, 255, 0.85)';
+export const GLASS_BACKDROP_BLUR = 'blur(20px)';
